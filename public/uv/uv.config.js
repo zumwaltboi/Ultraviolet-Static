6820108962887a69246ab96a400a5b621ee10db8
zumwaltboi <92687549+zumwaltboi@users.noreply.github.com>
2023-11-07 14:15:04 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "https://bare-server.doge-math-pages.byethost33.com/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
